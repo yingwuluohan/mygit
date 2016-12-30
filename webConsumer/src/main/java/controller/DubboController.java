07b7c073928af16740555c0dfa1d874df2ef6b20
unknown <dongfangnan@Koolearn-inc.com>
2016-12-30 10:24:03 +0800

    test

--- webConsumer/src/main/java/controller/DubboController.java
+++ webConsumer/src/main/java/controller/DubboController.java
@@ -6,18 +6,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * Created by dongfangnan on 2016/3/24.
  */
 @Controller
-@RequestMapping(value = "/test", method = {RequestMethod.GET, RequestMethod.POST})
+@RequestMapping(value = "/test" )
 public class DubboController {
 
 
 
-   /* @RequestMapping(value = "/getInfo", method = {RequestMethod.GET})
-    void getInfo(){
-        getHttpRequest.getAreaStr();
-    }*/
+    @RequestMapping(value = "/getInfo", method = {RequestMethod.GET})
+    public void getInfo(HttpServletRequest request , HttpServletResponse response ){
+
+    }
 
 }
